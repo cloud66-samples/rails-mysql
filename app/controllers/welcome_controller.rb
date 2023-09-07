6d2950470b2a510484d5ef83b1ac28466e7ac4d8
dima <dima@cloud66.com>
2023-09-07 20:49:30 +0200

    Enqueue jobs on random queues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,52 @@
 class WelcomeController < ApplicationController
   def index
-    SidekiqHighJob.perform_async
-    DelayedJobHighJob.new.deliver
-    Resque.enqueue(ResqueHighJob)
+    sidekiq_class_from_queue(random_queue).perform_async
+    delayed_job_class_from_queue(random_queue).new.deliver
+    Resque.enqueue(resque_class_from_queue(random_queue))
+  end
+
+  private
+
+  def random_queue
+    return ["high", "medium", "low"].sample
+  end
+
+  def sidekiq_class_from_queue(queue)
+    case queue
+    when "high"
+      return SidekiqHighJob
+    when "medium"
+      return SidekiqMediumJob
+    when "low"
+      return SidekiqLowJob
+    else
+      raise ::ArgumentError.new("Unknown queue #{queue}")
+    end
+  end
+
+  def resque_class_from_queue(queue)
+    case queue
+    when "high"
+      return ResqueHighJob
+    when "medium"
+      return ResqueMediumJob
+    when "low"
+      return ResqueLowJob
+    else
+      raise ::ArgumentError.new("Unknown queue #{queue}")
+    end
+  end
+
+  def delayed_job_class_from_queue(queue)
+    case queue
+    when "high"
+      return DelayedJobHighJob
+    when "medium"
+      return DelayedJobMediumJob
+    when "low"
+      return DelayedJobLowJob
+    else
+      raise ::ArgumentError.new("Unknown queue #{queue}")
+    end
   end
 end
