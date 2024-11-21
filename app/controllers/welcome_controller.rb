2ab87ee031978e392c1f39694c876f1256f7b51f
DimitriosLisenko <DimitriosLisenko@users.noreply.github.com>
2024-11-21 14:20:10 +0000

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @environment_variables = ""
     ENV.each_pair do |key, value|
       @environment_variables = @environment_variables + "#{key}: #{value} </br>"
     end
