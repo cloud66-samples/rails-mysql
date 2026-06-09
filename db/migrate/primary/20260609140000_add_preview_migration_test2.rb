a02b6cbd8eb3541a19e5342a61cfc5e12d06c574
Brent Graham <brent.graham@gmail.com>
2026-06-09 14:18:52 +0100

    Add test_col3 to articles @cloud66-migrate

--- db/migrate/primary/20260609140000_add_preview_migration_test2.rb
+++ db/migrate/primary/20260609140000_add_preview_migration_test2.rb
@@ -1,4 +1,4 @@
-class AddPreviewMigrationTest2 < ActiveRecord::Migration[7.1]
+class AddPreviewMigrationTest2 < ActiveRecord::Migration[6.0]
   def change
     add_column :articles, :test_col3, :string
   end
