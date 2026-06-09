81373b0437f2143f29d7abd19379962ea8dbd8cd
Brent Graham <brent.graham@gmail.com>
2026-06-09 15:14:39 +0100

    Changed version
    Use version 6.0 instead of 7.1

--- db/migrate/primary/20260609150000_add_preview_migration_test3.rb
+++ db/migrate/primary/20260609150000_add_preview_migration_test3.rb
@@ -1,4 +1,4 @@
-class AddPreviewMigrationTest3 < ActiveRecord::Migration[7.1]
+class AddPreviewMigrationTest3 < ActiveRecord::Migration[6.0]
   def change
     add_column :articles, :test_col4, :string
   end
